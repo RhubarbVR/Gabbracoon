03537d8dfd5ee925b5729ce8eb60281d5a6a0baa
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2023-05-12 00:21:22 -0400

    Start of Whispertail

---
+++ Client/Whispertail/MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.LifecycleEvents;
+
+using Whispertail_Shared;
+
+using System.Net.Http;
+using System.Reflection;
+
+namespace Whispertail
+{
+	public static class MauiProgram
+	{
+		public static void UpdateTheme(bool isLight) {
+			Console.WriteLine("Theme Change");
+			LightModeManager.IsLightMode = isLight;
+			LightModeManager.Update();
+		}
+
+		public static MauiApp CreateMauiApp() {
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts => fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"));
+			
+			LightModeManager.UseWeb = false;
+			builder.Services.AddScoped<LightModeManager>();
+
+			UpdateTheme(Application.Current.RequestedTheme == AppTheme.Light);
+			Application.Current.RequestedThemeChanged += (e, a) => UpdateTheme(a.RequestedTheme == AppTheme.Light);
+
+			builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+			builder.Services.AddBlazorWebViewDeveloperTools();
+#endif		
+			return builder.Build();
+		}
+	}
+}
